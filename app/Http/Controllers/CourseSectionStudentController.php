7fc57cf3db1ad1785905219ca56d241de3585bb1
蔡羽宣 <3a832055@gm.student.ncut.edu.tw>
2021-11-16 20:02:18 +0800

    model CourseSectionStudent --all

---
+++ app/Http/Controllers/CourseSectionStudentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CourseSectionStudent;
+use Illuminate\Http\Request;
+
+class CourseSectionStudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\CourseSectionStudent  $courseSectionStudent
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CourseSectionStudent $courseSectionStudent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\CourseSectionStudent  $courseSectionStudent
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CourseSectionStudent $courseSectionStudent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\CourseSectionStudent  $courseSectionStudent
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CourseSectionStudent $courseSectionStudent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\CourseSectionStudent  $courseSectionStudent
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CourseSectionStudent $courseSectionStudent)
+    {
+        //
+    }
+}
